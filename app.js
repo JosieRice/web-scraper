e05c6df9d9105ddb06d3466417470cc3b2af25df
Craig Rice <craigricetong@gmail.com>
2019-08-11 08:51:35 -0700

    added puppeteer dependency

--- app.js
+++ app.js
@@ -1,5 +1,29 @@
 const express = require('express')
 const app = express()
+const puppeteer = require("puppeteer");
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  const url =
+    "https://www.buzzfeed.com/pierceabernathy/bacon-avocado-brussels-sprout-salad-with-lemon-vinaigrette?utm_term=.oaavdq2mM#.sbezB7ndD";
+  await page.goto(url);
+  // await page.screenshot({path: 'example.png'});
+
+  const h1 = await page.evaluate(() =>
+    Array.from(document.querySelectorAll("h1")).map(title => title.innerText)
+  );
+
+  // const Ingredients = await page.evaluate(() =>
+  //   // grabs the whole ingredients block
+  //   Array.from(document.querySelectorAll("#mod-subbuzz-text-1"))
+  // );
+
+  console.log("h1", h1);
+  // console.log("Ingredients", Ingredients);
+
+  await browser.close();
+})();
 
 let port = process.env.PORT;
 if (port == null || port == "") {
