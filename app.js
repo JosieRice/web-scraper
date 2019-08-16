15c693127afa3768b70d4d5b00cb5a5799e2f1d5
Craig Rice <craigricetong@gmail.com>
2019-08-15 20:54:41 -0700

    debugging

--- app.js
+++ app.js
@@ -30,34 +30,35 @@ const buzzFeed = async (input) => {
   console.log("in buzzfeed function")
   const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
   const page = await browser.newPage();
-  const url = input;
-  try {
-    await page.goto(url);
-  } catch (e) {
-    console.log(e)
-  }
+  // const url = input;
+  // try {
+  await page.goto(url);
+  // } catch (e) {
+  //   console.log(e)
+  // }
 
 
-  const title = await page.evaluate(() =>
-    Array.from(document.querySelectorAll("h1")).map(title => title.innerText)
-  );
+  // const title = await page.evaluate(() =>
+  //   Array.from(document.querySelectorAll("h1")).map(title => title.innerText)
+  // );
 
-  const ingredients = await page.evaluate(() =>
-    Array.from(document.getElementById("mod-subbuzz-text-1").children).map(title => title.innerText)
-  );
+  // const ingredients = await page.evaluate(() =>
+  //   Array.from(document.getElementById("mod-subbuzz-text-1").children).map(title => title.innerText)
+  // );
 
-  const instructions = await page.evaluate(() =>
-    Array.from(document.getElementById("mod-subbuzz-text-2").children).map(title => title.innerText)
-  );
+  // const instructions = await page.evaluate(() =>
+  //   Array.from(document.getElementById("mod-subbuzz-text-2").children).map(title => title.innerText)
+  // );
 
-  let jsonRecipe = {
-    'title': title,
-    'ingredients': ingredients,
-    'instructions': instructions
-  }
+  // let jsonRecipe = {
+  //   'title': title,
+  //   'ingredients': ingredients,
+  //   'instructions': instructions
+  // }
 
-  await browser.close();
-  return jsonRecipe
+  // await browser.close();
+  // return jsonRecipe
+  return "from buzzfeed"
 };
 
 // take a recipe URL and return the recipe info
@@ -71,12 +72,12 @@ app.post('/api/v1/recipes', async (req, res) => {
   }
 
   const url = req.body.url;
-  // const recipe = await buzzFeed(url);
+  const recipe = await buzzFeed(url);
 
   return res.status(201).send({
     success: "true",
     message: `sent ${url} => here's what we found`,
-    recipe: "recipe"
+    recipe
   })
 });
 
